af42d4af8fafc401ce31d3e49e4ad3312c6c9b75
Joe <j-fortey@live.co.uk>
2020-07-07 21:17:23 +0100

    Added enemy and behaviour

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Stats")]
+    public float moveSpeed;
+
+    [Header("Target")]
+    public float chaseRange;
+    public float attackRange;
+    private Player player;
+
+    // components
+    private Rigidbody2D rig;
+
+    private void Awake()
+    {
+        // get the player target
+        player = FindObjectOfType<Player>();
+
+        // get the rigid body component
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float playerDist = Vector2.Distance(transform.position, player.transform.position);
+        if (playerDist <= attackRange)
+        {
+            // attack the player
+
+            rig.velocity = Vector2.zero;
+        }
+        // if we're in the chase range,  chase after the player
+        else if (playerDist <= chaseRange)
+        {
+            Chase();
+        }
+        else
+        {
+            rig.velocity = Vector2.zero;
+        }
+    }
+
+    // move towards the player
+    private void Chase()
+    {
+        // calculate direction between us and the player
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+
+        rig.velocity = direction * moveSpeed;
+    }
+}
